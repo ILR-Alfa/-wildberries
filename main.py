d9bea5ce0c252b590f6e5a6f096350afbc6fd2ed
ILR <verbar865@gmail.com>
2025-04-26 20:44:26 +0500

    данные из категорий

--- main.py
+++ main.py
@@ -7,8 +7,31 @@ from retry import retry
 
 
 
-def get_catalogs() -> dict:
+def get_catalogs_wb() -> dict:
     url =  'https://static-basket-01.wbbasket.ru/vol0/data/main-menu-ru-ru-v3.json'
     headers = {'Accept': '*/*', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     return requests.get(url, headers=headers).json()
 
+
+def get_data_category(catalogs_wb: dict) -> list:
+    catalog_data = []
+    if isinstance(catalogs_wb, dict) and "childs" not in catalogs_wb:
+        catalog_data.append({
+            'name': f"{catalogs_wb['name']}",
+            'shard': catalogs_wb.get('shards', None),
+            'url': catalogs_wb['url'],
+            'query': catalogs_wb.get('query', None)
+        })
+    elif isinstance(catalogs_wb, dict):
+        catalog_data.append({
+            'name': f"{catalogs_wb['name']}",
+            'shard': catalogs_wb.get('shard', None),
+            'url': catalogs_wb['url'],
+            'query': catalogs_wb.get('query', None)
+        })
+        catalog_data.extend(get_data_category(catalogs_wb['childs']))
+    else:
+        for child in catalogs_wb:
+            catalog_data.extend(get_data_category(child))
+    return catalog_data
+    
